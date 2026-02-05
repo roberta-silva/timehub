428fdcb04ac91267b332a07eb8d954e60740b087
Roberta Silva <robertadasilva.contato@gmail.com>
2026-02-05 16:34:55 -0300

    Implementa responsividade e menu mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 import FullscreenToggle from "./modules/fullscreen.js";
 import ThemeToggle from "./modules/tema.js";
+import MenuMobile from "./modules/menu-mobile.js";
 
 const fullscreen = new FullscreenToggle({
   botao: "#btn-expandir",
@@ -12,3 +13,6 @@ fullscreen.init();
 
 const tema = new ThemeToggle("#btn-tema", 'body');
 tema.init();
+
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="lista"]');
+menuMobile.init();
